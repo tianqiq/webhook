1edd8dc11028f38d51e03a7bb6b8d7dfdae4eb1b
DavidNineRoc <1033404553@qq.com>
2018-08-24 17:29:38 +0800

    格式化代码

--- index.php
+++ index.php
@@ -10,59 +10,89 @@ $config = [
 // 3. 验证程序是否上锁，防止重复操作
 // 4. 上锁文件
 // ($app = new Application(...array_values($config)))->validateToken()->validateLock()->lock();
-($app = new Application(...array_values($config)))->validateLock()->lock();
+
+try {
+
+    ($app = new Application(...array_values($config)))->validateLock()->lock();
+} catch (Exception $e) {
+
+    file_put_contents(
+        $this->log,
+        sprintf("[%s] %s %s", date('Y-m-d H:i:s'), $e->getMessage(), PHP_EOL),
+        FILE_APPEND
+    );
+
+    exit(0);
+}
+
+// 运行爬取
 $app->run();
+
+
 class Application
 {
     /**
      * 日志文件
+     *
      * @var string
      */
-    protected $log = 'log.txt';
+    protected $log = 'cache/log.txt';
+
     /**
      * 提交记录文件
+     *
      * @var string
      */
-    protected $history = 'history.txt';
+    protected $history = 'cache/history.txt';
+
     /**
      * 项目锁文件，防止冲突
+     *
      * @var string
      */
-    protected $lockFile = '.lock.txt';
+    protected $lockFile = 'cache/.lock.txt';
+
     /**
      * 项目根目录
+     *
      * @var string
      */
     protected $basePath;
+
     /**
      * token 验证
      *
      * @var string
      */
     protected $token;
+
     /**
      * 项目文件夹
      *
      * @var string
      */
     protected $target;
+
     /**
      * linux 用户
      *
      * @var string
      */
     protected $linuxUser;
+
     public function __construct(string $basePath, string $token, string $target, string $linuxUser)
     {
         $this->basePath = realpath($basePath);
         $this->token = $token;
         $this->target = $target;
         $this->linuxUser = $linuxUser;
-        $this->lockFile = $this->basePath . DIRECTORY_SEPARATOR . $this->lockFile;
-        $this->log = $this->basePath . DIRECTORY_SEPARATOR . $this->log;
-        $this->history = $this->basePath . DIRECTORY_SEPARATOR . $this->history;
-        $this->registerException();
+
+        $this->lockFile = $this->basePath.DIRECTORY_SEPARATOR.$this->lockFile;
+        $this->log = $this->basePath.DIRECTORY_SEPARATOR.$this->log;
+        $this->history = $this->basePath.DIRECTORY_SEPARATOR.$this->history;
     }
+
+
     /**
      * 验证 token 合法性
      *
@@ -72,12 +102,14 @@ class Application
     public function validateToken()
     {
         $json = json_decode($stream = file_get_contents('php://input'), true);
-        
-   if (! isset($json['token']) || $json['token'] != $this->token) {
-            throw new Exception('token validate fail, [' . $stream . ']');
+
+        if (! isset($json['token']) || $json['token'] != $this->token) {
+            throw new Exception('token validate fail, ['.$stream.']');
         }
+
         return $this;
     }
+
     /**
      * 验证是否上锁
      *
@@ -89,8 +121,10 @@ class Application
         if (is_file($this->lockFile)) {
             throw new Exception('token validate fail');
         }
+
         return $this;
     }
+
     /**
      * 上锁，防止重复请求
      */
@@ -98,50 +132,32 @@ class Application
     {
         file_put_contents($this->lockFile, ' ');
     }
-    public function __destruct()
-    {
-       if (is_file($this->lockFile)) {
-           unlink($this->lockFile);
-       }
-    }
+
+
     /**
-     * 注册异常捕获
-     */
-    protected function registerException()
-    {
-        set_exception_handler(function (Exception $e) {
-            file_put_contents(
-               $this->log,
-                sprintf("[%s] %s %s", date('Y-m-d H:i:s'), $e->getMessage(), PHP_EOL),
-                FILE_APPEND
-            );
-        });
-    }
-    /**
-     * @return string
-     * @throws Exception
+     * 运行 git 更新
      */
     public function run()
     {
-      if (! is_dir($this->target)) {
-         throw new Exception("[{$this->target}] path not exists");
-      }
-      $cmd = "cd {$this->target} && sudo -Hu {$this->linuxUser} git pull origin master";
+        $cmd = "cd {$this->target} && sudo -Hu {$this->linuxUser} git pull origin master";
         $result = shell_exec($cmd);
-        // 把成功继续写到文件
+
+        // 成功记录写到文件
         file_put_contents(
             $this->history,
             sprintf('%s %s %s %s %s %s', str_repeat('#', 40), PHP_EOL, $result, PHP_EOL, str_repeat('#', 40), PHP_EOL),
             FILE_APPEND
         );
     }
+
+
     /**
-     * 当前是不是 linux 系统
-     *
-     * @return bool
+     * 删除上锁文件
      */
-    protected function isLinux()
+    public function __destruct()
     {
-        return PHP_OS === 'Linux';
+        if (is_file($this->lockFile)) {
+            unlink($this->lockFile);
+        }
     }
 }
